58dc537d73a57de6e64a859e31882774ed81e56b
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-02-05 13:36:25 +0000

    [Finish]認証機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
