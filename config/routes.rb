2282ec16d90bd86db18366d9f6b0046f1152a903
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-02-05 13:58:07 +0000

    [Finish]管理者としての機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
