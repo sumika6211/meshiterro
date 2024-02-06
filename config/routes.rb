fd6c4911cf71449e652326a861c1b2f1d1f68ee7
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-02-06 11:47:31 +0000

    [Finish]構成の改善

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,16 @@ Rails.application.routes.draw do
     get 'dashboards', to: 'dashboards#index'
     resources :users, only: [:destroy]
   end
-  devise_for :users
-  root to: 'homes#top'
-  get "homes/about" => "homes#about", as: "about"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
-  resources :users, only: [:show, :edit, :update]
+
 end
